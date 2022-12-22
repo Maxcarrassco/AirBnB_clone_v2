2ed2875620cd51591b15fcdd2987791911e11b3c
Maxwell D. Dorliea <deboy170@gmail.com>
2022-12-22 15:34:13 +0000

    Added task 1 solution

---
+++ 1-pack_web_static.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""ALX SE First Fabric Module."""
+from datetime import datetime
+from fabric.api import local
+
+
+def do_pack():
+    """Generate a .tgz archive from the contents of the web_static."""
+    """Folder of your AirBnB Clone."""
+    time = datetime.now()
+    time = datetime.strftime(time, "%Y%m%d%H")
+    path = f"versions/name_{time}.tgz"
+    local("mkdir -p versions")
+    result = local(f"tar -cvzf {path} web_static")
+
+    if result.succeeded:
+        return path
