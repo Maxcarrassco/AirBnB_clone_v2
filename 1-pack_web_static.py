f6efd77c84f9c4b3d3acb77d4d99b44771ed43bd
Maxwell D. Dorliea <deboy170@gmail.com>
2022-12-22 15:40:10 +0000

    Fixed bug in task 1 solution

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,7 +8,7 @@ def do_pack():
     """Generate a .tgz archive from the contents of the web_static."""
     """Folder of your AirBnB Clone."""
     time = datetime.now()
-    time = datetime.strftime(time, "%Y%m%d%H%S")
+    time = datetime.strftime(time, "%Y%m%d%H%M%S")
     path = f"versions/name_{time}.tgz"
     local("mkdir -p versions")
     result = local(f"tar -cvzf {path} web_static")
