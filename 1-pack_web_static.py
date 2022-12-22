5b75b104e862b2f2f800301ce69b2600eb81fef6
Maxwell D. Dorliea <deboy170@gmail.com>
2022-12-22 15:51:30 +0000

    Fixed bug in task 1 solution

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,10 +7,12 @@ from fabric.api import local
 def do_pack():
     """Generate a .tgz archive from the contents of the web_static."""
     """Folder of your AirBnB Clone."""
+    result = local("mkdir -p versions")
+    if result.failed:
+        return;
     time = datetime.now()
     time = datetime.strftime(time, "%Y%m%d%H%M%S")
     path = f"versions/name_{time}.tgz"
-    local("mkdir -p versions")
     result = local(f"tar -cvzf {path} web_static")
 
     if result.succeeded:
