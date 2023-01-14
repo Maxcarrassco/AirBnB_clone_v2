98d4f6ad9a038a0f2b8e2d2075893c6211635deb
Maxwell D. Dorliea <deboy170@gmail.com>
2023-01-14 17:18:41 +0000

    Added task 7 solution

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""This module defines a class to manage file storage for hbnb clone."""
 import json
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
+    """This class manages storage of hbnb models in JSON format."""
     __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a dictionary of models currently in storage"""
+        """Return a dictionary of models currently in storage."""
         if not cls:
             return FileStorage.__objects
         objs = {}
@@ -19,11 +19,11 @@ class FileStorage:
         return objs
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
+        """Add new object to storage dictionary."""
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
-        """Saves storage dictionary to file"""
+        """Save storage dictionary to file."""
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
             temp.update(FileStorage.__objects)
@@ -32,7 +32,7 @@ class FileStorage:
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """Load storage dictionary from file."""
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -62,3 +62,7 @@ class FileStorage:
         obj = obj.to_dict()
         key = f"{obj['__class__']}.{obj['id']}"
         del FileStorage.__objects[key]
+
+    def close(self):
+        """Close the current session."""
+        self.reload()
